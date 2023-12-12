b3658729450fc7d054bdffca7fc3e03d8ef27592
Ashraf266 <mohamedashrafece@gmail.com>
2023-12-12 16:31:05 +0200

    Explaination comment for localhost

--- 01-Simple Client-Server application based on UDP/UDP_Client.py
+++ 01-Simple Client-Server application based on UDP/UDP_Client.py
@@ -1,14 +1,15 @@
 import socket
-#the server is on the same PC
+#the server is on the same PC -> same ip address ... 'localhost' == ip address of the server(PC ip address)
 serverName = 'localhost'
 #server port number 12000
 serverPort = 12000
 #AF_INET -> internet (IP) , SOCK_DGRAM -> UDP .... Automatically given a port number by the OS
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 message = input('Input lowercase sentense: ')
-#sending the data to the localhost on the port: 12000 
+#sending the data to the localhost(IP address of this PC) on the port: 12000
+#sendto(message, (ip, port)) 
 clientSocket.sendto(message.encode('UTF-8'), (serverName, serverPort))
-#receive the modified data from the server
+#receive the modified data from the server  .... 2048 max bytes to receive
 data, clientAddress = clientSocket.recvfrom(2048)
 #print the data
 print(data.decode('UTF-8'))
